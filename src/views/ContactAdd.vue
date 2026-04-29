9e4c972120f96a19279cd347a55ec896ef2398ae
hoangphi <philh@onland.tech>
2026-04-29 11:12:45 +0700

    Tao trang them moi lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="page">
-    <p>Trang thêm mới</p>
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="emptyContact" @submit:contact="createContact" />
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-export default { name: "ContactAdd" };
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+  components: { ContactForm },
+  data() {
+    return {
+      emptyContact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+      message: "",
+    };
+  },
+  methods: {
+    async createContact(data) {
+      try {
+        await ContactService.create(data);
+        alert("Liên hệ được thêm thành công.");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+        this.message = "Đã có lỗi xảy ra khi thêm liên hệ.";
+      }
+    },
+  },
+};
 </script>
