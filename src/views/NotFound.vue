2651b1eab2be6979e1c29bddce94d8205e6d768f
hoangphi <philh@onland.tech>
2026-04-29 11:01:39 +0700

    Cai dat trang loi 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="page">
-    <p>
-      Oops, không thể tìm thấy trang. Trở về
-      <router-link to="/">trang chủ.</router-link>
+    <p> Oops, không thể tìm thấy trang. Trở về <router-link to="/">trang chủ.</router-link>
     </p>
   </div>
 </template>
